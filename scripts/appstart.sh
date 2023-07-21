09ed2b4e844cdf41b7064082428eb82c8737eec2
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2023-07-21 12:11:56 +0530

    first commit

---
+++ scripts/appstart.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ #npm start /home/ubuntu/node/server/server.js
+
+ cd /home/ubuntu/interview_admin
+
+ #pm2 restart interview_node 
---
+++ scripts/installpkgs.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /home/ubuntu/interview_admin
+
+# sudo npm install
